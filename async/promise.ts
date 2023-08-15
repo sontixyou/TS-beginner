e8fa5ed53f29b0c1ef3892d866525b91c5a5f341
kenken <kengoviolinphoto@gmail.com>
2023-08-15 21:34:31 +0900

    add type

--- async/promise.ts
+++ async/promise.ts
@@ -1,38 +1,38 @@
 // 非同期でAPIにリクエストを投げて値を取得する処理
-function request1() {
-  return new Promise((resolve) => {
-    setTimeout(() => {
+function request1() : Promise<number> {
+  return new Promise((resolve)  => {
+    setTimeout(() : void => {
       resolve(1);
     }, 1000);
   });
 }
 
 // 受け取った値を別のAPIにリクエストを投げて値を取得する処理
-function request2(result1) {
+function request2(result1 : number) : Promise<number> {
   return new Promise((resolve) => {
-    setTimeout(() => {
+    setTimeout(() : void => {
       resolve(result1 + 1);
     }, 1000);
   });
 }
 
 // 受け取った値を別のAPIにリクエストを投げて値を取得する処理
-function request3(result2) {
+function request3(result2 : number) : Promise<number> {
   return new Promise((resolve) => {
-    setTimeout(() => {
+    setTimeout(() : void => {
       resolve(result2 + 2);
     }, 1000);
   });
 }
 
 request1()
-  .then((result1) => {
+  .then((result1 : number) : Promise<number> => {
     return request2(result1);
   })
-  .then((result2) => {
+  .then((result2 : number) : Promise<number> => {
     return request3(result2);
   })
-  .then((result3) => {
+  .then((result3 : number) : void => {
     console.log(result3);
     // @log: 4
   });
